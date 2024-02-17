c307beb97d4c62d7361e7830c2c0a163fad472af
hiba_neh <107880774+hibounashi@users.noreply.github.com>
2024-02-17 20:19:03 +0100

    section 1 done ✔

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+            'custom-black': '#333333',
+            'custom-blue': '#2D9CDB',
+            'custom-light': '#F2F2F2',
+            'custom-gray': '#E0E0E0',
+            'custom-green': '#27AE60',
+            'custom-red': '#EB5757',
+            'custom-subtitle-gray': '#828282',
+            'custom-footer-background': '#100E1D',
+            },
+        },
+        },
+    };
+    
